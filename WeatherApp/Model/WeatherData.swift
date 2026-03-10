50da0fc3cca7a06e0c38e42232e16e88ff7b1f37
kartikey <kartikeyeagarwal1234@gmail.com>
2026-03-10 18:26:54 +0530

    Completion of API calls and Json parsing

---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by REAL  on 10/03/26.
+//
+
+import Foundation
---
+++ WeatherApp/Model/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by REAL  on 10/03/26.
+//
+
+import Foundation
